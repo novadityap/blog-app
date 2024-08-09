9e489d904def6b40022ce0e7aca6cd6120fa1e04
novadityap <novaaditya23@gmail.com>
2024-08-09 18:59:58 +0700

    feat: add signup validation schema

---
+++ server/validations/userValidation.js
@@ -0,0 +1,11 @@
+import joi from "joi";
+
+const signupSchema = joi.object({
+  username: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
+
+export {
+  signupSchema
+};
