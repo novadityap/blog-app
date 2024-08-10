6662486fdf25ad25e89331b9c75db9427813dbfb
novadityap <novaaditya23@gmail.com>
2024-08-10 14:13:36 +0700

    feat: add mail configuration

---
+++ server/utils/sendMail.js
@@ -0,0 +1,19 @@
+import transporter from "../config/mail.js";
+import logger from "../config/logger.js";
+
+const sendMail = async (email, subject, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.MAIL_FROM,
+      to: email,
+      subject,
+      html
+    });
+  
+    logger.info(`Email has been sent to ${email}: ${info.messageId}`);
+  } catch(e) {
+    logger.error(e);
+  }
+}
+
+export default sendMail;
