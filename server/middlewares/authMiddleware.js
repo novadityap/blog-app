9465661f08e6e353d9033c59214e812b6bb0c23e
novadityap <novaaditya23@gmail.com>
2024-08-21 20:59:26 +0700

    feat: add jwt error response to error middleware and add auth middleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import ResponseError from "../utils/responseError.js";
+import logger from "../config/logger.js";
+import jwt from "jsonwebtoken";
+const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.replace('Bearer ', '');
+    if (!token) {
+      logger.info('unauthorized: no access token provided');
+      throw new ResponseError('Token is not provided', 401);
+    }
+
+    req.user = jwt.verify(token, process.env.JWT_SECRET); 
+
+    logger.info(`authenticate success: user ${req.user.userId} is authenticated`);
+    next();
+  } catch (e) {
+    next(e);
+  }
+}
+
+
+export default authMiddleware;
