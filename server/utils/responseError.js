7b9f21a0135e09bfa0bc66301932d032476e6490
novadityap <novaaditya23@gmail.com>
2024-09-05 16:16:51 +0700

    refactor: add optional data property for ResponseError class

--- server/utils/responseError.js
+++ server/utils/responseError.js
@@ -1,8 +1,9 @@
 class ResponseError extends Error {
-  constructor(message, code, errors = null) {
+  constructor(message, code, errors = null, data = null) {
     super(message);
     this.code = code;
     this.errors = errors;
+    this.data = data;
   }
 }
 
