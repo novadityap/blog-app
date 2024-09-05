e3485ba064716f1a18793b721370dff45bcc956d
novadityap <novaaditya23@gmail.com>
2024-09-05 12:05:32 +0700

    refactor: improve error middleware to handle error codes and messages more consintently

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -1,25 +1,38 @@
-import ResponseError from '../utils/responseError.js';
-import jwt from 'jsonwebtoken';
-import logger from '../config/logger.js';
-import isJson from '../utils/isJson.js';
+  import ResponseError from '../utils/responseError.js';
+  import jwt from 'jsonwebtoken';
+  import logger from '../utils/logger.js';
 
-const errorMiddleware = (err, req, res, next) => {
-  const isBadRequest = err.status === 400;
-  const isJwtExpired = err instanceof jwt.TokenExpiredError;
-  const isJwtError = err instanceof jwt.JsonWebTokenError;
+  const errorMiddleware = (err, req, res, next) => {
+    const { code = 500, message = 'Internal server Error', errors = null } = err;
 
-  if (isBadRequest) {
-    if (isJson(err.message)) return res.status(err.status).json({ error: JSON.parse(err.message) });
-    return res.status(err.status).json({ error: err.message });
-  } else if (err instanceof ResponseError && !isBadRequest) {
-    return res.status(err.status).json({ error: err.message });
-  } else if (isJwtExpired || isJwtError) {
-    logger.info(`Token is invalid or expired: ${err.message}`);
-    return res.status(401).json({ error: 'Token is invalid or expired' });
-  } else {
+    if (err instanceof jwt.TokenExpiredError || err instanceof jwt.JsonWebTokenError) {
+      logger.info(`Token error - ${err.message}`);
+      return res.status(code).json({ 
+        code,
+        message: 'Token is invalid or expired' 
+      });
+    }
+
+    if (code === 400) {
+      return res.status(code).json({ 
+        code,
+        message,
+        errors
+      });
+    }
+
+    if (err instanceof ResponseError && code !== 400 && code !== 500) {
+      return res.status(code).json({ 
+        code,
+        message 
+      });
+    }
+
     logger.error(err);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+    return res.status(500).json({ 
+      code: 500,
+      message: 'Internal server error' 
+    });
+  };
 
-export default errorMiddleware;
+  export default errorMiddleware;
