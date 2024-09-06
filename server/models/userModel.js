b92975dd1656987b849179869c26d18d69f2a967
novadityap <novaaditya23@gmail.com>
2024-09-07 05:34:02 +0700

    refactor: rename isAdmin field to role in user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -15,9 +15,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  isAdmin: {
-    type: Boolean,
-    default: false
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
   },
   avatar: {
     type: String,
