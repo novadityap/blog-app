cdd8790a19f2ed2c44b3888215ff83429fd6d97e
novadityap <novaaditya23@gmail.com>
2024-09-06 14:50:41 +0700

    refactor: rename authMiddleware import to verifyTokenMiddleware

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -6,7 +6,7 @@ import {
   signout,
   refreshToken,
 } from '../controllers/authController.js';
-import authMiddleware from '../middlewares/authMiddleware.js';
+import verifyTokenMiddleware from '../middlewares/verifyTokenMiddleware.js';
 import express from 'express';
 
 const router = express.Router();
@@ -15,7 +15,7 @@ router.post('/signup', signup);
 router.post('/email-verification/:verificationToken', emailVerification);
 router.post('/resend-email-verification', resendEmailVerification);
 router.post('/signin', signin);
-router.post('/signout', authMiddleware, signout);
+router.post('/signout', verifyTokenMiddleware, signout);
 router.post('/refresh-token', refreshToken);
 
 export default router;
