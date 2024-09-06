13196dc431bddd4b9b738e2870b52b48ec674ed3
novadityap <novaaditya23@gmail.com>
2024-09-07 05:35:12 +0700

    refactor: update import path for authorize middleware

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -6,7 +6,7 @@ import {
   signout,
   refreshToken,
 } from '../controllers/authController.js';
-import verifyTokenMiddleware from '../middlewares/verifyTokenMiddleware.js';
+import authorizeMiddleware from '../middlewares/authorizeMiddleware.js';
 import express from 'express';
 
 const router = express.Router();
@@ -15,7 +15,7 @@ router.post('/signup', signup);
 router.post('/email-verification/:verificationToken', emailVerification);
 router.post('/resend-email-verification', resendEmailVerification);
 router.post('/signin', signin);
-router.post('/signout', verifyTokenMiddleware, signout);
+router.post('/signout', authorizeMiddleware(['admin', 'user']), signout);
 router.post('/refresh-token', refreshToken);
 
 export default router;
