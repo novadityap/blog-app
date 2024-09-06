06618a4330e7f94824c94b42a3ba40e64f492703
novadityap <novaaditya23@gmail.com>
2024-09-07 05:30:40 +0700

    feat: add 404 not found error response handler

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -1,10 +1,10 @@
-  import ResponseError from '../utils/responseError.js';
-  import jwt from 'jsonwebtoken';
-  import logger from '../utils/logger.js';
+import ResponseError from '../utils/responseError.js';
+import jwt from 'jsonwebtoken';
+import logger from '../utils/logger.js';
 
-  const errorMiddleware = (err, req, res, next) => {
+const errorMiddleware = (err, req, res, next) => {
   const { code = 500, message = 'Internal server Error', errors = null, data = null } = err;
-
+  
     if (err instanceof jwt.TokenExpiredError || err instanceof jwt.JsonWebTokenError) {
       logger.info(`Token error - ${err.message}`);
       return res.status(401).json({ 
@@ -21,7 +21,15 @@
       });
     }
 
-    if (err instanceof ResponseError && code !== 400 && code !== 500) {
+    if (code === 404) {
+      return res.status(code).json({
+        code,
+        message,
+        data
+      })
+    }
+
+    if (err instanceof ResponseError && code !== 500) {
       return res.status(code).json({ 
         code,
         message 
