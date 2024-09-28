913680a72ce8f44a037e076b658125219a7af2bd
novadityap <novaaditya23@gmail.com>
2024-09-28 16:10:25 +0700

    chore: migrate from Flowbite UI to Shadcn UI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
-const flowbite = require('flowbite-react/tailwind');
-
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
   ],
   theme: {
-    extend: {},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
   plugins: [
-    flowbite.plugin()
-  ],
+      require("tailwindcss-animate")
+],
 }
 
