8c4176adbf2d825dd143a7e031eababb4759a72b
novadityap <novaaditya23@gmail.com>
2024-10-23 13:36:45 +0700

    Refactor authorize and check owner function and move into src folder

---
+++ server/src/middlewares/authorize.js
@@ -0,0 +1,33 @@
+import Role from '../models/roleModel.js';
+import logger from '../utils/logger.js';
+import ResponseError from '../utils/responseError.js';
+
+const authorize = (action, resource) => {
+  return async (req, res, next) => {
+    const { id: currentUserId, roles: currentUserRoles } = req.user;
+
+    try {
+      const roles = await Role.find({ name: { $in: currentUserRoles } }).populate('permissions');
+
+      const hasPermission = roles.some(role =>
+        role.permissions.some(
+          permission =>
+            permission.action === action && permission.resource === resource
+        )
+      );
+
+      if (!hasPermission) {
+        logger.info(
+          `permission denied - user ${currentUserId} does not have permission to ${action} ${resource}`
+        );
+        throw new ResponseError('Permission denied', 403);
+      };
+
+      return next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+export default authorize;
---
+++ server/src/utils/loadModel.js
@@ -0,0 +1,9 @@
+import path from 'node:path';
+const loadModel = async (modelName) => {
+  const modelPath = path.join(process.cwd(), 'models', `${modelName}Model.js`);
+  const model = await import(modelPath);
+
+  return model.default;
+}
+
+export default loadModel;
