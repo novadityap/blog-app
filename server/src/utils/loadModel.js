43549529efd74513b2d8a9a4f29a4a0432f99b78
novadityap <novaaditya23@gmail.com>
2024-11-11 22:19:02 +0700

    Fix incorrect model path reference

--- server/src/utils/loadModel.js
+++ server/src/utils/loadModel.js
@@ -1,6 +1,6 @@
 import path from 'node:path';
 const loadModel = async (modelName) => {
-  const modelPath = path.join(process.cwd(), 'models', `${modelName}Model.js`);
+  const modelPath = path.join(process.cwd(), 'src/models', `${modelName}Model.js`);
   const model = await import(modelPath);
 
   return model.default;
