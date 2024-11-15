6ed336bc574042c8ba30f76241ebee005439ff31
novadityap <novaaditya23@gmail.com>
2024-11-15 09:22:37 +0700

    Refactor code readability and remove category seeder

--- server/src/seeders/seed.js
+++ server/src/seeders/seed.js
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 import seedPermission from "./permissionSeeder.js";
 import seedRole from "./roleSeeder.js";
 import seedUser from "./userSeeder.js";
-import seedCategory from "./categorySeeder.js";
 import logger from "../utils/logger.js";
 import connectDB from '../config/connection.js';
 const seed = async () => {
   try {
     await connectDB();
-    await seedCategory();
     await seedPermission();
     await seedRole();
     await seedUser();
