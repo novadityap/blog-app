ff7cf98f1eb798cc4e8ff38c63e202ee8737b414
novadityap <novaaditya23@gmail.com>
2024-12-20 22:36:50 +0700

    Refactor seed files

--- server/src/seeders/permissionSeeder.js
+++ server/src/seeders/permissionSeeder.js
@@ -1,34 +1,24 @@
 import Permission from '../models/permissionModel.js';
+import logger from '../utils/logger.js';
 
 const seedPermission = async () => {
-  const resources = [
-    'permission',
-    'role',
-    'user',
-    'post',
-    'comment',
-    'category',
-    'dashboard',
-  ];
-  const actions = ['create', 'read', 'update', 'delete'];
-
-  const exceptions = {
-    post: ['read'],
-    dashboard: ['create', 'update', 'delete'],
+  const resources = {
+    role: ['create', 'update', 'remove', 'search', 'show'],
+    user: ['create', 'update', 'remove', 'search', 'show'],
+    post: ['create', 'update', 'remove', 'like'],
+    comment: ['create', 'update', 'remove', 'search', 'show', 'list'],
+    category: ['create', 'update', 'remove', 'search', 'show', 'list'],
+    permission: ['list'],
+    dashboard: ['stats'],
   };
 
-  const permissions = resources.flatMap(resource =>
-    actions
-      .filter(action => !(exceptions[resource] || []).includes(action))
-      .map(action => ({
-        action,
-        resource,
-        description: `permission to ${action} ${resource}`,
-      }))
+  const permissions = Object.entries(resources).flatMap(([resource, actions]) =>
+    actions.map((action) => ({ name: `${action}_${resource}` }))
   );
 
-  await Permission.deleteMany({});
+  await Permission.deleteMany();
   await Permission.insertMany(permissions);
+  logger.info('permissions seeded successfully');
 };
 
 export default seedPermission;
--- server/src/seeders/roleSeeder.js
+++ server/src/seeders/roleSeeder.js
@@ -1,29 +1,32 @@
 import Role from '../models/roleModel.js';
 import Permission from '../models/permissionModel.js';
+import logger from '../utils/logger.js';
 
 const seedRole = async () => {
   const permissions = await Permission.find();
-  const userPermissionsCriteria = {
-    user: ['read', 'update', 'delete'],
-    comment: ['create', 'update', 'delete'],
-  };
+  const userPermissions = [
+    'show_user',
+    'update_user',
+    'remove_user',
+    'create_comment',
+    'remove_comment',
+    'like_post',  
+  ];
+
   const roles = [
     { name: 'admin', permissions: [] },
     { name: 'user', permissions: [] },
   ];
 
-  for (let permission of permissions) {
-    const { _id, action, resource } = permission;
-
-    roles[0].permissions.push(_id);
-
-    if (userPermissionsCriteria[resource]?.includes(action)) {
-      roles[1].permissions.push(_id);
-    }
+    for (let permission of permissions) {
+    roles[0].permissions.push(permission._id);
+    if (userPermissions.includes(permission.name)) roles[1].permissions.push(permission._id);
   }
 
-  await Role.deleteMany({});
+  await Role.deleteMany();
   await Role.insertMany(roles);
+
+  logger.info('roles seeded successfully');
 };
 
 export default seedRole;
