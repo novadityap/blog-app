af296851f9077d08268707f405da54014a0f0ba1
novadityap <novaaditya23@gmail.com>
2024-12-20 22:31:59 +0700

    Refactor validation schemas with Mongoose integration

--- server/src/validations/categoryValidation.js
+++ server/src/validations/categoryValidation.js
@@ -1,10 +1,24 @@
 import Joi from 'joi';
+import mongoose from 'mongoose';
 
-const nameSchema = Joi.string().required();
-
-const baseCategorySchema = Joi.object({
-  name: nameSchema
+const categorySchema = Joi.object({
+  name: Joi.string().required(),
 });
 
-export const createCategorySchema = baseCategorySchema;
-export const updateCategorySchema = baseCategorySchema.fork(['name'], schema => schema.optional())
+export const searchCategorySchema = Joi.object({
+  page: Joi.number().integer().positive().min(1).default(1),
+  limit: Joi.number().integer().positive().min(1).max(100).default(10),
+  search: Joi.string().allow('').optional(),
+});
+export const getCategorySchema = Joi.string()
+  .custom((value, helpers) => {
+    if (!mongoose.Types.ObjectId.isValid(value))
+      return helpers.message('Category id is invalid');
+    return value;
+  })
+  .label('categoryId')
+  .required();
+export const createCategorySchema = categorySchema;
+export const updateCategorySchema = categorySchema.fork(['name'], schema =>
+  schema.optional()
+);
--- server/src/validations/postValidation.js
+++ server/src/validations/postValidation.js
@@ -1,18 +1,29 @@
 import Joi from 'joi';
 import mongoose from 'mongoose';
 
-const titleSchema = Joi.string().required();
-const contentSchema = Joi.string().required();
-const categorySchema = Joi.string().custom((value, helpers) => {
-  if (!mongoose.Types.ObjectId.isValid(value)) return helpers.error('invalid category id');
-  return value;
-}).required();
-
-const basePostSchema = Joi.object({
-  title: titleSchema,
-  category: categorySchema,
-  content: contentSchema,
+const postSchema = Joi.object({
+  title: Joi.string().required(),
+  content: Joi.string().required(),
+  category: Joi.string().custom((value, helpers) => {
+    if (!mongoose.Types.ObjectId.isValid(value)) return helpers.message('Invalid category id');
+    return value;
+  }).label('category').required()
 });
 
-export const createPostSchema = basePostSchema;
-export const updatePostSchema = basePostSchema;
+export const searchPostSchema = Joi.object({
+  page: Joi.number().integer().positive().min(1).default(1),
+  limit: Joi.number().integer().positive().min(1).max(100).default(10),
+  search: Joi.string().allow('').optional(),
+  category: Joi.string().custom((value, helpers) => {
+    if (!mongoose.Types.ObjectId.isValid(value)) return helpers.message('Invalid category id');
+    return value;
+  }).optional()
+});
+
+export const getPostSchema = Joi.string().custom((value, helpers) => {
+  if (!mongoose.Types.ObjectId.isValid(value)) return helpers.message('Invalid post id');
+  return value;
+}).label('postId').required();
+
+export const createPostSchema = postSchema;
+export const updatePostSchema = postSchema.fork(['title', 'content', 'category'], schema => schema.optional());
