161429ead433c40381b057c38ddc843215411944
novadityap <novaaditya23@gmail.com>
2024-12-21 06:07:00 +0700

    Refactor test utility functions to enhance user, role, permission, comment, and post creation, and improve file handling

--- server/tests/testUtil.js
+++ server/tests/testUtil.js
@@ -1,33 +1,32 @@
-import * as fs from 'node:fs/promises';
-import path from 'node:path';
+import { readFile, unlink, readdir, stat } from 'node:fs/promises';
 import User from '../src/models/userModel.js';
 import Category from '../src/models/categoryModel.js';
 import Post from '../src/models/postModel.js';
+import Permisssion from '../src/models/permissionModel.js';
+import Role from '../src/models/roleModel.js';
+import Comment from '../src/models/commentModel.js';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import path from 'node:path';
 
-const uploadDirs = {
-  avatars: process.env.AVATAR_UPLOADS_DIR,
-  posts: process.env.POST_UPLOADS_DIR,
-};
-
-export const createTestUser = async () => {
+export const createTestUser = async (fields = {}) => {
   return await User.create({
     username: 'test',
     email: 'test@me.com',
-    password: 'test123',
+    password: await bcrypt.hash('test123', 10),
+    ...fields,
   });
 };
 
-export const createManyTestUsers = async count => {
+export const createManyTestUsers = async () => {
   const users = [];
 
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < 15; i++) {
     users.push(
-      User.create({
+      createTestUser({
         username: `test${i}`,
         email: `test${i}@me.com`,
-        password: 'test123',
       })
     );
   }
@@ -35,43 +34,95 @@ export const createManyTestUsers = async count => {
   return await Promise.all(users);
 };
 
-export const removeAllTestUsers = async () => {
+export const removeTestUser = async () => {
   await User.deleteMany({ username: { $regex: /^test\d*/ } });
 };
 
-export const removeTestUser = async () => {
-  await User.findOneAndDelete({ username: 'test' });
-};
-
-export const getTestUser = async () => {
-  return await User.findOne({ username: 'test' });
-};
-
-export const createTestCategory = async () => {
-  return await Category.create({
-    name: 'test',
+export const createTestComment = async (fields = {}) => {
+  const user = await createTestUser();
+  return await Comment.create({
+    text: 'test',
+    userId: user._id,
+    ...fields,
   });
 };
 
-export const createManyTestCategories = async count => {
-  const categories = [];
+export const createManyTestComments = async () => {
+  const comments = [];
 
-  for (let i = 0; i < count; i++) {
-    categories.push(
-      Category.create({
+  for (let i = 0; i < 15; i++) {
+    comments.push(
+      createTestComment({
+        text: `test${i}`,
+      })
+    );
+  }
+
+  return await Promise.all(comments);
+};
+
+export const removeTestComment = async () => {
+  await Comment.deleteMany({ text: { $regex: /^test\d*/ } });
+};
+
+export const createTestRole = async (fields = {}) => {
+  return await Role.create({
+    name: 'test',
+    ...fields,
+  });
+};
+
+export const createManyTestRoles = async () => {
+  const roles = [];
+
+  for (let i = 0; i < 15; i++) {
+    roles.push(
+      createTestRole({
         name: `test${i}`,
       })
     );
+  }
+
+  return await Promise.all(roles);
+};
+
+export const removeTestRole = async () => {
+  await Role.deleteMany({ name: { $regex: /^test\d*/ } });
+};
+
+export const createTestPermission = async (fields = {}) => {
+  return await Permisssion.create({
+    name: 'test',
+    ...fields,
+  });
+};
+
+export const removeTestPermission = async () => {
+  await Permisssion.deleteMany({ name: { $regex: /^test\d*/ } });
+};
+
+export const createTestCategory = async (fields = {}) => {
+  return await Category.create({
+    name: 'test',
+    ...fields,
+  });
+};
+
+export const createManyTestCategories = async () => {
+  const categories = [];
+
+  for (let i = 0; i < 15; i++) {
+    categories.push(createTestCategory({ name: `test${i}` }));
   }
 
   return await Promise.all(categories);
 };
 
 export const removeTestCategory = async () => {
-  await Category.findOneAndDelete({ name: 'test' });
+  await Category.deleteMany({ name: { $regex: /^test\d*/ } });
 };
 
-export const createTestPost = async () => {
+export const createTestPost = async (fields = {}) => {
   const user = await createTestUser();
   const category = await createTestCategory();
 
@@ -79,48 +130,41 @@ export const createTestPost = async () => {
     title: 'test',
     slug: 'test',
     content: 'test',
-    user: user._id,
+    userId: user._id,
     category: category._id,
+    ...fields,
   });
 };
 
-export const createManyTestPosts = async count => {
-  const users = await createManyTestUsers(count);
-  const categories = await createManyTestCategories(count);
+export const createManyTestPosts = async () => {
+  const users = await createManyTestUsers();
+  const categories = await createManyTestCategories();
   const posts = [];
 
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < 15; i++) {
     posts.push(
-      Post.create({
+      createTestPost({
         title: `test${i}`,
         slug: `test${i}`,
-        content: 'test',
-        user: users[i]._id,
+        content: `test${i}`,
+        userId: users[i]._id,
         category: categories[i]._id,
       })
     );
   }
 
-  return await Promise.all(users);
+  return await Promise.all(posts);
 };
 
 export const removeTestPost = async () => {
-  await Post.findOneAndDelete({ title: 'test' });
-};
-
-export const getTestPost = async () => {
-  return await Post.findOne({ title: 'test' });
-};
-
-export const removeAllTestPosts = async () => {
   await Post.deleteMany({ title: { $regex: /^test\d*/ } });
 };
 
-export const createAuthToken = type => {
+export const createToken = (type, role, userId) => {
   return jwt.sign(
     {
-      id: new mongoose.Types.ObjectId(),
-      roles: ['admin'],
+      id: userId || new mongoose.Types.ObjectId(),
+      roles: [role],
     },
     type === 'auth' ? process.env.JWT_SECRET : process.env.JWT_REFRESH_SECRET,
     {
@@ -132,53 +176,41 @@ export const createAuthToken = type => {
   );
 };
 
-export const fileExists = async (directory, filename) => {
-  const filePath = path.join(process.cwd(), uploadDirs[directory], filename);
-
-  try {
-    await fs.access(filePath);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-export const copyFile = async (source, directory) => {
-  const filename = path.basename(source);
-  const destinationPath = path.join(
-    process.cwd(),
-    uploadDirs[directory],
-    filename
-  );
-
-  await fs.copyFile(source, destinationPath);
-};
-
-export const removeFile = async (directory, filename) => {
-  const directories = {
-    avatars: process.env.AVATAR_UPLOADS_DIR,
-    posts: process.env.POST_UPLOADS_DIR,
-  };
-  const filePath = path.join(process.cwd(), directories[directory], filename);
-
-  await fs.unlink(filePath);
-};
-
-export const readLogs = async () => {
+const readLog = async () => {
   const path = `${process.cwd()}/src/logs/app-${
     new Date().toISOString().split('T')[0]
   }.log`;
-  const log = await fs.readFile(path, 'utf-8');
+  const logs = await readFile(path, 'utf-8');
 
-  return log
+  return logs
     .trim()
     .split('\n')
     .map(line => JSON.parse(line));
 };
 
-export const findRelevantLog = async (message, startTime) => {
-  const logs = await readLogs();
+export const findLog = async (message, startTime) => {
+  const logs = await readLog();
   return logs.find(log => {
     return new Date(log.timestamp) > startTime && log.message === message;
   });
+};
+
+export const removeTestFile = async type => {
+  const exceptionFilename = 'default.jpg';
+  const folder =
+    type === 'avatar'
+      ? path.resolve(process.env.AVATAR_DIR)
+      : path.resolve(process.env.POST_DIR);
+  const files = await readdir(folder);
+
+  const deletionPromises = files
+    .filter(file => file !== exceptionFilename)
+    .map(async file => {
+      const filePath = path.join(folder, file);
+
+      const stats = await stat(filePath);
+      if (stats.isFile()) await unlink(filePath);
+    });
+
+  await Promise.all(deletionPromises);
 };
