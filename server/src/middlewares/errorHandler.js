aae856c4af7d990d45ddb9c694fd4029e79641df
novadityap <novaaditya23@gmail.com>
2024-12-20 22:40:43 +0700

    Refactor error handling in JWT authentication

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -6,27 +6,29 @@ const errorHandler = (err, req, res, next) => {
     code = 500,
     message = 'Internal server Error',
     errors,
-    data,
     stack,
   } = err;
 
-  const logMethod = code >= 500 ? 'error' : 'warn';
-  logger.log(logMethod, message, { stack, ...req.metadata });
-
-  if (err instanceof jwt.TokenExpiredError || err instanceof jwt.JsonWebTokenError) {
-    return res.status(401).json({
-      code: 401,
-      message: 'Token expired',
-    });
+  if (err instanceof jwt.JsonWebTokenError) {
+    logger.warn('token is invalid');
+    return res.status(401).json({ code: 401, message: 'Token is invalid' });
   }
 
-  const response = { code, message };
-
-  if (code === 400 && errors) {
-    response.errors = errors;
-  } else if (code === 404) {
-    response.data = data;
+  if (err instanceof jwt.TokenExpiredError) {
+    logger.warn('token has expired');
+    return res.status(401).json({ code: 401, message: 'Token has expired' });
   }
+
+  logger.log(code >= 500 ? 'error' : 'warn', message, { stack, ...req.metadata });
+
+  const response = {
+    code,
+    message: code >= 500 ? 'Internal server error' : message,
+    ...(code === 400 && { errors }),
+    ...(code === 409 && { errors }),
+    ...(code === 404 && { data : null }),
+    ...(process.env.NODE_ENV === 'development' && code >= 500 && { stack }),
+  };
 
   res.status(code).json(response);
 };
