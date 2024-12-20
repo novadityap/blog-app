bf405fca92dcf1f718d7b42d413de66434750476
novadityap <novaaditya23@gmail.com>
2024-12-21 06:06:11 +0700

    Refactor dashboard controller to improve statistics retrieval and enhance data aggregation for posts and comments

--- server/src/controllers/dashboardController.js
+++ server/src/controllers/dashboardController.js
@@ -2,46 +2,98 @@ import Post from '../models/postModel.js';
 import User from '../models/userModel.js';
 import Comment from '../models/commentModel.js';
 import Role from '../models/roleModel.js';
-import Permission from '../models/permissionModel.js';
 import Category from '../models/categoryModel.js';
 import logger from '../utils/logger.js';
 
-export const getDashboardData = async (req, res, next) => {
+export const stats = async (req, res, next) => {
   try {
     const totalUsers = await User.countDocuments();
-    const totalPosts = await Post.countDocuments();
-    const totalComments = await Comment.countDocuments();
     const totalRoles = await Role.countDocuments();
-    const totalPermissions = await Permission.countDocuments();
     const totalCategories = await Category.countDocuments();
 
-    const recentPosts = await Post.find()
-      .sort({ createdAt: -1 })
-      .limit(5)
-      .populate('userId category', 'username name');
-    const recentComments = await Comment.find()
-      .sort({ createdAt: -1 })
-      .limit(5)
-      .populate('userId postId', 'username title');
+    const [{ totalPosts, recentPosts }] = await Post.aggregate()
+      .facet({
+        totalPosts: [{ $count: 'count' }],
+        recentPosts: [
+          { $sort: { createdAt: -1 } },
+          { $limit: 5 },
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'userId',
+              foreignField: '_id',
+              as: 'user',
+              pipeline: [{ $project: { username: 1 } }],
+            },
+          },
+          {
+            $lookup: {
+              from: 'categories',
+              localField: 'category',
+              foreignField: '_id',
+              as: 'category',
+              pipeline: [{ $project: { name: 1 } }],
+            },
+          },
+        ],
+      })
+      .project({
+        totalPosts: {
+          $ifNull: [{ $arrayElemAt: ['$totalPosts.count', 0] }, 0],
+        },
+        recentPosts: 1,
+      });
 
+    const [{ totalComments, recentComments }] = await Comment.aggregate()
+      .facet({
+        totalComments: [{ $count: 'count' }],
+        recentComments: [
+          { $sort: { createdAt: -1 } },
+          { $limit: 5 },
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'userId',
+              foreignField: '_id',
+              as: 'user',
+              pipeline: [{ $project: { username: 1 } }],
+            },
+          },
+          {
+            $lookup: {
+              from: 'posts',
+              localField: 'postId',
+              foreignField: '_id',
+              as: 'post',
+              pipeline: [{ $project: { title: 1 } }],
+            },
+          },
+        ],
+      })
+      .project({
+        totalComments: {
+          $ifNull: [{ $arrayElemAt: ['$totalComments.count', 0] }, 0],
+        },
+        recentComments: 1,
+      });
 
-    logger.info(`fetch dashboard data success`);
+    logger.info('statistics data retrieved successfully');
     res.json({
       code: 200,
-      message: 'Dashboard data retrieved successfully',
+      message: 'Statistics data retrieved successfully',
       data: {
         totalUsers,
         totalPosts,
         totalComments,
         totalRoles,
-        totalPermissions,
         totalCategories,
         recentPosts,
         recentComments,
       },
     });
-  } catch (err) {
-    next(err);
+  } catch (e) {
+    next(e);
   }
 };
 
+export default { stats };
