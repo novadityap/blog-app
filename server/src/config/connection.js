121c7268a805926602c04251fcf1574c2f4c2f7b
novadityap <novaaditya23@gmail.com>
2024-12-20 22:08:58 +0700

    Update connection string handling

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -3,10 +3,14 @@ import logger from "../utils/logger.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    logger.info(`Database connected successfully`);
+    const mongoUri = process.env.NODE_ENV === 'development' ? process.env.TEST_MONGO_URI : process.env.MONGO_URI;
+
+    await mongoose.connect(mongoUri);
+    logger.info('Database connected successfully');
   } catch(e) {
-    logger.error(`Database connection failed - ${e.message}`);
+    console.log(e);
+    logger.error('Database connection failed');
+    process.exit(1);
   }
 }
 
