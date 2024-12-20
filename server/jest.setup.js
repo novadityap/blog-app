989c94a4ff2e59cb3c51be02d28a59d6e5cdee03
novadityap <novaaditya23@gmail.com>
2024-12-21 06:06:38 +0700

    Enhance Jest setup for improved database connection and seeding, including global token creation for tests

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,1 +1,22 @@
 import 'dotenv/config';
+import connectDB from './src/config/connection.js';
+import mongoose from 'mongoose';
+import seedPermission from './src/seeders/permissionSeeder.js';
+import seedRole from './src/seeders/roleSeeder.js';
+import { createToken } from './tests/testUtil.js';
+
+beforeAll(async () => {
+  await connectDB();
+  await mongoose.connection.db.dropDatabase();
+  await seedPermission();
+  await seedRole();
+
+  global.userToken = createToken('auth', 'user');
+  global.adminToken = createToken('auth', 'admin');
+  global.adminRefreshToken = createToken('refresh', 'admin');
+  global.validObjectId = new mongoose.Types.ObjectId();
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
