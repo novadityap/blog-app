2a4bf23230e0fab01ee6ae7bfe94e075d3f05fef
novadityap <novaaditya23@gmail.com>
2024-12-22 21:41:51 +0700

    Update @types/node and undici-types to latest versions; adjust tailwind.config.js import style

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,17 +7,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+        borderRadius: {
+            lg: 'var(--radius)',
+            md: 'calc(var(--radius) - 2px)',
+            sm: 'calc(var(--radius) - 4px)'
+        },
+        colors: {}
+    }
   },
-  plugins: [
-      require("tailwindcss-animate")
+plugins: [
+    import("tailwindcss-animate")
 ],
 }
 
