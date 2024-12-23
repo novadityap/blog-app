96d7b24ba8549c2c26520f6c1e68b551d3be40ee
novadityap <novaaditya23@gmail.com>
2024-12-24 00:07:31 +0700

    Enhance error formatting by adding message capitalization for improved readability

--- server/src/utils/formatError.js
+++ server/src/utils/formatError.js
@@ -1,9 +1,15 @@
 const formatError = errors => {
   return errors.reduce((acc, curr) => {
-    const message = curr.message.replace(/"/g, '');
+    let message = curr.message.replace(/"/g, '');
+    message = capitalizeMessage(message);
+    
     acc[curr.context.label.replace(/\[0\]/g, '')] = message;
     return acc;
   }, {});
 };
+
+const capitalizeMessage = message => {
+  return message.charAt(0).toUpperCase() + message.slice(1);
+}
 
 export default formatError;
