c98b8d11b97b996d0fdb119220bd7772b85f513f
novadityap <novaaditya23@gmail.com>
2024-12-24 16:27:49 +0700

    Enhance error formatting by adding camel case message formatting and improving capitalization

--- server/src/utils/formatError.js
+++ server/src/utils/formatError.js
@@ -1,15 +1,20 @@
 const formatError = errors => {
   return errors.reduce((acc, curr) => {
     let message = curr.message.replace(/"/g, '');
-    message = capitalizeMessage(message);
+    message = formatCamelCaseMessage(message);
+    message = capitalizeFirstWord(message);
     
     acc[curr.context.label.replace(/\[0\]/g, '')] = message;
     return acc;
   }, {});
 };
 
-const capitalizeMessage = message => {
-  return message.charAt(0).toUpperCase() + message.slice(1);
-}
+const capitalizeFirstWord = message => {
+  return message.charAt(0).toUpperCase() + message.slice(1).toLowerCase();
+};
+
+const formatCamelCaseMessage = message => {
+  return message.replace(/([a-z])([A-Z])/g, '$1 $2');
+};
 
 export default formatError;
