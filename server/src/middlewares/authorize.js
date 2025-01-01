c7f417fcb2ee9c8b536e8dde44034ebe903f4ab1
novadityap <novaaditya23@gmail.com>
2025-01-01 18:11:06 +0700

    Refactor authorization middleware to simplify role-based permission checks

--- server/src/middlewares/authorize.js
+++ server/src/middlewares/authorize.js
@@ -1,24 +1,16 @@
-import Role from '../models/roleModel.js';
 import logger from '../utils/logger.js';
 import ResponseError from '../utils/responseError.js';
 
-const authorize = (permission) => {
+const authorize = (roles) => {
   return async (req, res, next) => {
-    const { roles: currentUserRoles } = req.user;
-
     try {
-      const roles = await Role.find({ 
-      name : { $in: currentUserRoles } }).populate('permissions');
-
-      const hasPermission = roles.some(role =>
-        role.permissions.some(rolePermission => rolePermission.name === permission)
-      );
-      if (!hasPermission) {
-        logger.warn('permission denied');
-        throw new ResponseError('Permission denied', 403);
+      if (roles.includes(req.user.role)) {
+        logger.info('permission granted');
+        return next();
       };
 
-      return next();
+      logger.warn('permission denied');
+      throw new ResponseError('Permission denied', 403);
     } catch (e) {
       next(e);
     }
