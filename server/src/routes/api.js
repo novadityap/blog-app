a39336c54d6ce6578ad3aafcbe4600d5b3d74700
novadityap <novaaditya23@gmail.com>
2025-01-01 18:10:59 +0700

    Refactor API routes to enforce admin authorization for category, comment, post, role, and user endpoints

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,7 +3,6 @@ import categoryController from '../controllers/categoryController.js';
 import commentController from '../controllers/commentController.js';
 import postController from '../controllers/postController.js';
 import dashboardController from '../controllers/dashboardController.js';
-import permissionController from '../controllers/permissionController.js';
 import roleController from '../controllers/roleController.js';
 import userController from '../controllers/userController.js';
 import authController from '../controllers/authController.js';
@@ -19,9 +18,7 @@ apiRouter.post('/auth/resend-verification', authController.resendVerification);
 apiRouter.post('/auth/signin', authController.signin);
 apiRouter.post('/auth/request-reset-password', authController.requestResetPassword);
 apiRouter.post('/auth/reset-password/:token', authController.resetPassword);
-
 apiRouter.get('/categories/list', categoryController.list);
-
 apiRouter.get('/posts', postController.search);
 apiRouter.get('/posts/:postId', postController.show);
 
@@ -33,44 +30,42 @@ apiRouter.post('/auth/refresh-token', authController.refreshToken);
 apiRouter.post('/auth/signout', authController.signout);
 
 // Category API
-apiRouter.post('/categories', authorize('create_category'), categoryController.create);
-apiRouter.get('/categories', authorize('search_category'), categoryController.search);
-apiRouter.get('/categories/:categoryId', categoryController.show);
-apiRouter.put('/categories/:categoryId', authorize('update_category'), categoryController.update);
-apiRouter.delete('/categories/:categoryId', authorize('remove_category'), categoryController.remove
+apiRouter.post('/categories', authorize(['admin']), categoryController.create);
+apiRouter.get('/categories', authorize(['admin']), categoryController.search);
+apiRouter.get('/categories/:categoryId', authorize(['admin']), categoryController.show);
+apiRouter.put('/categories/:categoryId', authorize(['admin']), categoryController.update);
+apiRouter.delete('/categories/:categoryId', authorize(['admin']), categoryController.remove
 );
 
 // Comment API
-apiRouter.get('/comments', authorize('search_comment'), commentController.search);
-apiRouter.post('/posts/:postId/comments', authorize('create_comment'), commentController.create);
-apiRouter.get('/posts/:postId/comments/:commentId', authorize('show_comment'), commentController.show);
-apiRouter.patch('/posts/:postId/comments/:commentId', authorize('update_comment'), commentController.update);
-apiRouter.delete('/posts/:postId/comments/:commentId', authorize('remove_comment'), commentController.remove);
+apiRouter.get('/comments', authorize(['admin']), commentController.search);
+apiRouter.post('/posts/:postId/comments', authorize(['admin', 'user']), commentController.create);
+apiRouter.get('/posts/:postId/comments/:commentId', authorize(['admin']), commentController.show);
+apiRouter.patch('/posts/:postId/comments/:commentId', authorize(['admin']), commentController.update);
+apiRouter.delete('/posts/:postId/comments/:commentId', authorize(['admin', 'user']), commentController.remove);
 
 // Post API
-apiRouter.post('/posts', authorize('create_post'), postController.create);
-apiRouter.put('/posts/:postId', authorize('update_post'), postController.update);
-apiRouter.patch('/posts/:postId/like', authorize('like_post'), postController.like);
-apiRouter.delete('/posts/:postId', authorize('remove_post'), postController.remove);
+apiRouter.post('/posts', authorize(['admin']), postController.create);
+apiRouter.put('/posts/:postId', authorize(['admin']), postController.update);
+apiRouter.patch('/posts/:postId/like', authorize(['admin', 'user']), postController.like);
+apiRouter.delete('/posts/:postId', authorize(['admin']), postController.remove);
 
 // Dashboard API
-apiRouter.get('/dashboard/', authorize('stats_dashboard'), dashboardController.stats);
-
-// Permission API
-apiRouter.get('/permissions/list', authorize('list_permission'), permissionController.list);
+apiRouter.get('/dashboard/', authorize(['admin']), dashboardController.stats);
 
 // Role API
-apiRouter.post('/roles', authorize('create_role'), roleController.create);
-apiRouter.get('/roles', authorize('search_role'), roleController.search);
-apiRouter.get('/roles/:roleId', authorize('show_role'), roleController.show);
-apiRouter.put('/roles/:roleId', authorize('update_role'), roleController.update);
-apiRouter.delete('/roles/:roleId', authorize('remove_role'), roleController.remove);
+apiRouter.post('/roles', authorize(['admin']), roleController.create);
+apiRouter.get('/roles', authorize(['admin']), roleController.search);
+apiRouter.get('/roles/list', authorize(['admin']), roleController.list);
+apiRouter.get('/roles/:roleId', authorize(['admin']), roleController.show);
+apiRouter.put('/roles/:roleId', authorize(['admin']), roleController.update);
+apiRouter.delete('/roles/:roleId', authorize(['admin']), roleController.remove);
 
 // User API
-apiRouter.get('/users/', authorize('search_user'), userController.search);
-apiRouter.post('/users/', authorize('create_user'), userController.create);
-apiRouter.get('/users/:userId', authorize('show_user'), userController.show);
-apiRouter.put('/users/:userId', authorize('update_user'), userController.update);
-apiRouter.delete('/users/:userId', authorize('remove_user'), userController.remove);
+apiRouter.get('/users/', authorize(['admin']), userController.search);
+apiRouter.post('/users/', authorize(['admin']), userController.create);
+apiRouter.get('/users/:userId', authorize(['admin', 'user']), userController.show);
+apiRouter.put('/users/:userId', authorize(['admin', 'user']), userController.update);
+apiRouter.delete('/users/:userId', authorize(['admin']), userController.remove);
 
 export default apiRouter;
