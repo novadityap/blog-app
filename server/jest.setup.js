7b5696f4f2ccc62dc60c887c726b5e0fa356d989
novadityap <novaaditya23@gmail.com>
2025-01-01 18:10:06 +0700

    Remove unused permission seeder from Jest setup

--- server/jest.setup.js
+++ server/jest.setup.js
@@ -1,14 +1,12 @@
 import 'dotenv/config';
 import connectDB from './src/config/connection.js';
 import mongoose from 'mongoose';
-import seedPermission from './src/seeders/permissionSeeder.js';
 import seedRole from './src/seeders/roleSeeder.js';
 import { createToken } from './tests/testUtil.js';
 
 beforeAll(async () => {
   await connectDB();
   await mongoose.connection.db.dropDatabase();
-  await seedPermission();
   await seedRole();
 
   global.userToken = createToken('auth', 'user');
