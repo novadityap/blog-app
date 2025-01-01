0125f2747aa94eb4ef8a1864300ea36e5d96589d
novadityap <novaaditya23@gmail.com>
2025-01-01 18:11:13 +0700

    Remove unused permissions field from role schema

--- server/src/models/roleModel.js
+++ server/src/models/roleModel.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const roleSchema = new mongoose.Schema({
   name: String,
-  permissions: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Permission'
-  }]
 }, {
   timestamps: true
 });
