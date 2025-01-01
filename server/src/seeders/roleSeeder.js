88591b046d47611410f0a66aee35a7cf7e5e9f95
novadityap <novaaditya23@gmail.com>
2025-01-01 18:10:24 +0700

    Refactor role seeder to simplify role creation and remove unused permission logic

--- server/src/seeders/roleSeeder.js
+++ server/src/seeders/roleSeeder.js
@@ -1,30 +1,10 @@
 import Role from '../models/roleModel.js';
-import Permission from '../models/permissionModel.js';
 import logger from '../utils/logger.js';
 
 const seedRole = async () => {
-  const permissions = await Permission.find();
-  const userPermissions = [
-    'show_user',
-    'update_user',
-    'remove_user',
-    'create_comment',
-    'remove_comment',
-    'like_post',  
-  ];
-
-  const roles = [
-    { name: 'admin', permissions: [] },
-    { name: 'user', permissions: [] },
-  ];
-
-    for (let permission of permissions) {
-    roles[0].permissions.push(permission._id);
-    if (userPermissions.includes(permission.name)) roles[1].permissions.push(permission._id);
-  }
-
+  const roles = ['admin', 'user'];
   await Role.deleteMany();
-  await Role.insertMany(roles);
+  await Role.insertMany(roles.map((role) => ({ name: role })));
 
   logger.info('roles seeded successfully');
 };
