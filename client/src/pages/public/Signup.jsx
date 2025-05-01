02b0a8b46169458df503a1430205867ed395eaa2
novadityap <novaaditya23@gmail.com>
2025-05-01 14:27:48 +0700

    Add Role management page with DataTable integration; implement role CRUD operations and enhance UI with shadcn-ui components

--- client/src/pages/public/Signup.jsx
+++ client/src/pages/public/Signup.jsx
@@ -1,102 +1,96 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Button } from '@/components/shadcn-ui/button';
+import { Input } from '@/components/shadcn-ui/input';
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from '@/components/shadcn-ui/alert';
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
   CardFooter,
-} from '@/components/ui/card';
+} from '@/components/shadcn-ui/card';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/components/shadcn-ui/form';
 import { TbLoader, TbCircleCheck } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
-import { cn } from '@/lib/utils';
 import { useSignupMutation } from '@/services/authApi';
 import useFormHandler from '@/hooks/useFormHandler';
 
 const Signup = () => {
-  const {
-    register,
-    handleSubmit,
-    isLoading,
-    isError,
-    error,
-    isSuccess,
-    message,
-  } = useFormHandler(useSignupMutation);
+  const { form, handleSubmit, isLoading, isSuccess, message } = useFormHandler({
+    mutation: useSignupMutation,
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
 
   return (
-    <div className="flex flex-col gap-y-4 items-center justify-center min-h-screen ">
-      <Card className="w-full sm:w-[450px]">
-        <CardHeader>
-          <CardTitle className="text-2xl font-bold text-green-500">
-            Sign Up
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          {isSuccess && (
-            <Alert variant="success" className="mb-4">
-              <TbCircleCheck className="size-5 text-green-500" />
-              <AlertTitle>Success</AlertTitle>
-              <AlertDescription>{message}</AlertDescription>
-            </Alert>
-          )}
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <Label htmlFor="username" className="text-gray-500">
-                Username
-              </Label>
-              <Input
-                type="username"
-                id="username"
-                {...register('username')}
-                required
-                placeholder="Enter your username"
-                className={cn(isError && 'border-red-200 focus:ring-red-200')}
-              />
-
-              {error?.errors?.username && (
-                <p className="text-red-500 text-sm">{error.errors.username}</p>
+    <Card className="w-full sm:w-[450px] my-10">
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold text-green-500">
+          Sign Up
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        {isSuccess && (
+          <Alert variant="success">
+            <TbCircleCheck className="size-5 text-green-500" />
+            <AlertTitle>Success</AlertTitle>
+            <AlertDescription>{message}</AlertDescription>
+          </Alert>
+        )}
+        <Form {...form}>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-            </div>
-
-            <div className="mb-4">
-              <Label htmlFor="email" className="text-gray-500">
-                Email
-              </Label>
-              <Input
-                type="email"
-                id="email"
-                {...register('email')}
-                required
-                placeholder="Enter your email"
-                className={cn(isError && 'border-red-200 focus:ring-red-200')}
-              />
-
-              {error?.errors?.email && (
-                <p className="text-red-500 text-sm">{error.errors.email}</p>
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-            </div>
-
-            <div className="mb-4">
-              <Label htmlFor="password" className="text-gray-500">
-                Password
-              </Label>
-              <Input
-                type="password"
-                id="password"
-                {...register('password')}
-                required
-                placeholder="Enter your password"
-                className={cn(isError && 'border-red-200 focus:ring-red-200')}
-              />
-
-              {error?.errors?.password && (
-                <p className="text-red-500 text-sm">{error.errors.password}</p>
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
               )}
-            </div>
-
+            />
             <Button
               variant="primary"
               type="submit"
@@ -113,26 +107,26 @@ const Signup = () => {
               )}
             </Button>
           </form>
-        </CardContent>
-        <CardFooter className="flex flex-col items-center">
-          <div className="flex items-center justify-betweens gap-1">
-            <p className="text-gray-500 text-sm">Have an account?</p>
-            <Link
-              to="/signin"
-              className="text-sm text-gray-500 hover:underline hover:text-blue-600"
-            >
-              Sign In
-            </Link>
-          </div>
+        </Form>
+      </CardContent>
+      <CardFooter className="flex flex-col items-center">
+        <div className="flex items-center justify-betweens gap-1">
+          <p className="text-gray-500 text-sm">Have an account?</p>
           <Link
-            to="/request-reset-password"
+            to="/signin"
             className="text-sm text-gray-500 hover:underline hover:text-blue-600"
           >
-            Forgot Password?
+            Sign In
           </Link>
-        </CardFooter>
-      </Card>
-    </div>
+        </div>
+        <Link
+          to="/request-reset-password"
+          className="text-sm text-gray-500 hover:underline hover:text-blue-600"
+        >
+          Forgot Password?
+        </Link>
+      </CardFooter>
+    </Card>
   );
 };
 
