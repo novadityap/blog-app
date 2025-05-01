00d0971c914a8e7d86a1e3f8d92995736ebc8ec1
novadityap <novaaditya23@gmail.com>
2025-05-01 14:24:36 +0700

    Refactor main.jsx to simplify routing and render App component directly; update index.css import for consistency

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,34 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import About from './pages/About.jsx';
-import Project from './pages/Project.jsx';
-import SignUp from './pages/SignUp.jsx';
-import SignIn from './pages/SignIn.jsx';
+import '@/index.css'
+import App from '@/App'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/projects' element={<Project />} />
-      <Route path='/signin' element={<SignIn />} />
-      <Route path='/signup' element={<SignUp />} />
-    </>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
