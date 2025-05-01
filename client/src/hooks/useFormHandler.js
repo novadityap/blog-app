fa7707aa95b444a1d34af556d9788270b5d8550f
novadityap <novaaditya23@gmail.com>
2025-05-01 14:35:36 +0700

    Refactor useFormHandler hook

--- client/src/hooks/useFormHandler.js
+++ client/src/hooks/useFormHandler.js
@@ -1,24 +1,68 @@
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setToken, setCurrentUser } from '@/features/authSlice';
+import { toast } from 'react-hot-toast';
 
-const useFormHandler = (mutationFn, token) => {
+const useFormHandler = ({
+  mutation,
+  tokens,
+  ids,
+  isDatatableForm,
+  onComplete,
+  defaultValues,
+}) => {
+  const dispatch = useDispatch();
   const [message, setMessage] = useState('');
-  const [mutate, { isLoading, isError, error, isSuccess }] = mutationFn();
-  const { register, handleSubmit, reset } = useForm();
+  const [mutate, { isLoading, isError, error, isSuccess }] = mutation();
+  const { handleSubmit, ...form } = useForm({ defaultValues });
 
   const onSubmit = async data => {
     try {
-      const payload = token ? { data, token } : data;
+      const payload = tokens
+        ? { data, ...tokens }
+        : ids
+        ? { data, ...ids }
+        : data;
+
       const result = await mutate(payload).unwrap();
-      setMessage(result.message);
-      reset();
+
+      if (result.data?.token) {
+        const { token, _id, ...user } = result.data;
+        dispatch(setToken(token));
+        dispatch(
+          setCurrentUser({
+            id: _id,
+            ...user,
+          })
+        );
+      }
+
+      if (isDatatableForm && onComplete) {
+        onComplete(result);
+      } else {
+        setMessage(result.message);
+      }
+
+      form.reset();
     } catch (e) {
-      if (e.code !== 400) setMessage(e.message);
+      if (e.errors) {
+        Object.keys(e.errors).forEach(key => {
+          const message = e.errors[key];
+          form.setError(key, { type: 'manual', message });
+        });
+      }
+
+      if (isDatatableForm && e.code !== 400 && e.code !== 409) {
+        toast.error(e.message);
+      } else {
+        setMessage(e.message);
+      }
     }
   };
 
   return {
-    register,
+    form,
     handleSubmit: handleSubmit(onSubmit),
     isLoading,
     isError,
