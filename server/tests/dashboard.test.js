0125107c2f870fd11321e51dd2ac222bb93e8c74
novadityap <novaaditya23@gmail.com>
2025-06-11 06:16:46 +0700

    Refactor dashboard tests to streamline setup and teardown processes

--- server/tests/dashboard.test.js
+++ server/tests/dashboard.test.js
@@ -1,33 +1,29 @@
 import request from 'supertest';
 import app from '../src/app.js';
 import {
+  createTestCategory,
+  createTestUser,
+  createTestRole,
+  createTestPost,
   createManyTestCategories,
   createManyTestPosts,
   createManyTestComments,
   createManyTestRoles,
   createManyTestUsers,
-  removeTestCategory,
-  removeTestPost,
-  removeTestComment,
-  removeTestRole,
-  removeTestUser,
+  removeAllTestCategories,
+  removeAllTestPosts,
+  removeAllTestComments,
+  removeAllTestRoles,
+  removeAllTestUsers,
 } from './testUtil.js';
 
 describe('GET /api/dashboard', () => {
-  beforeEach(async () => {
-    await createManyTestCategories();
-    await createManyTestPosts();
-    await createManyTestRoles();
-    await createManyTestUsers();
-    await createManyTestComments();
-  });
-
   afterEach(async () => {
-    await removeTestCategory();
-    await removeTestPost();
-    await removeTestComment();
-    await removeTestRole();
-    await removeTestUser();
+    await removeAllTestCategories();
+    await removeAllTestPosts();
+    await removeAllTestComments();
+    await removeAllTestRoles();
+    await removeAllTestUsers();
   });
 
   it('should return an error if user does not have permission', async () => {
@@ -40,13 +36,23 @@ describe('GET /api/dashboard', () => {
   });
 
   it('should return dashboard statistics data', async () => {
+    await createTestRole();
+    await createTestUser();
+    await createTestCategory();
+    await createTestPost();
+    await createManyTestCategories();
+    await createManyTestPosts();
+    await createManyTestRoles();
+    await createManyTestUsers();
+    await createManyTestComments();
+
     const result = await request(app)
       .get('/api/dashboard')
       .set('Authorization', `Bearer ${global.adminToken}`);
 
     expect(result.status).toBe(200);
     expect(result.body.message).toBe('Statistics data retrieved successfully');
-    expect(result.body.data.totalPosts).toBe(15);
+    expect(result.body.data.totalPosts).toBeGreaterThanOrEqual(15);
     expect(result.body.data.totalComments).toBe(15);
     expect(result.body.data.totalCategories).toBeGreaterThanOrEqual(15);
     expect(result.body.data.totalRoles).toBeGreaterThanOrEqual(15);
