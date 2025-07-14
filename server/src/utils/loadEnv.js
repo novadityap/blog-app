7cfe4bc869b90a8532708674f85fb0bd87d3500e
novadityap <novaaditya23@gmail.com>
2025-07-15 01:31:10 +0700

    Refactor seed script in package.json to remove NODE_ENV setting and update loadEnv function

--- server/src/utils/loadEnv.js
+++ server/src/utils/loadEnv.js
@@ -4,7 +4,8 @@ import path from 'node:path';
 const loadEnv = () => {
   const mode = process.env.NODE_ENV || 'development';
   const envFile = `.env.${mode}`;
-  dotenv.config({ path: path.resolve(`${envFile}`) });
+  
+  if (mode !== 'production') dotenv.config({ path: envFile }); 
 }
 
 export default loadEnv;
